4d273cce40448ecf59e7ebd1b9ebab6d39365fcf
JoaquinIbar <ibar.joaquin@gmail.com>
2021-10-12 20:35:55 -0300

    Working the HOME

--- js/packages/web/src/views/home/index.tsx
+++ js/packages/web/src/views/home/index.tsx
@@ -1,7 +1,9 @@
-import { Layout } from 'antd';
+import { Col, Layout, Row, Image } from 'antd';
 import React from 'react';
 import { useStore } from '@oyster/common';
 import { useMeta } from '../../contexts';
+
+const { Content } = Layout;
 
 export const HomeView = () => {
   const { isLoading, store } = useMeta();
@@ -12,6 +14,17 @@ export const HomeView = () => {
   return (
     <Layout style={{ margin: 0, marginTop: 30, alignItems: 'center' }}>
       <h1> ESTO ES LA FUCKING HOME </h1>
+      <Content>
+        <Row>
+          <Col>
+          
+                    </Col>
+          <Col>
+
+          </Col>
+        </Row>
+      </Content>
     </Layout>
   );
 };
+
